271f4e6c3d6657865cf07bd8d131e4b9eb51d00c
Marcelo Barbosa <mbarbosa@teksystems.com>
2021-02-18 16:27:06 -0600

    Testing unit test for creating method

--- america_app/tests.py
+++ america_app/tests.py
@@ -1,9 +1,21 @@
 from django.test import TestCase, Client
+from .models import *
 
 class AmericaLineTest(TestCase):
-    def test_homepage(self):
+    @classmethod
+    def setUpTestData(cls):
+        User.objects.create(
+            name = "marcelo",
+            password = "password", 
+            email = "marceloarthurb@gmail.com",
+
+        )
+    def test_API_login_page(self):
         client = Client()
         response = client.get('')
         self.assertEqual(response.status_code, 200)
-
-# Create your tests here.
+    def test_model_create(self):
+        user = User.objects.first()
+        self.assertEqual(user.name, "marcelo")
+        self.assertEqual(user.password, "password")
+        self.assertEqual(user.email, "marceloarthurb@gmail.com")
