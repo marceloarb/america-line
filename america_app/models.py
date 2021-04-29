504442d2bc8dfb5137df1f4d6063edb46fe76e2e
Marcelo Barbosa <mbarbosa@teksystems.com>
2021-04-29 14:00:23 -0500

    deleting the date from the models and views

--- america_app/models.py
+++ america_app/models.py
@@ -6,14 +6,9 @@ import datetime
 class UserManager(models.Manager):
     def basic_validator(self, requestPOST):
         errors = {}
-        print(requestPOST['date'])
-        if datetime.datetime.now().strftime('%Y-%m-%d') <= requestPOST['date']:
-            errors['date'] = "Date needs to be past date "
         if len(requestPOST['name']) < 3:
             errors['name'] = "Name is too short"
-        users_with_name = User.objects.filter(name=requestPOST['name'])
-        if len(users_with_name) > 0:
-            errors['duplicate'] = "Name already taken"
+        users_with_name = User.objects.filter(name=requestPOST['name']) 
         if len(requestPOST['password']) < 8:
             errors['password'] = "Password is too short"
         if requestPOST['password'] != requestPOST['password_conf']:
@@ -27,7 +22,6 @@ class User(models.Model):
     name = models.TextField()
     password = models.TextField()
     email = models.CharField(max_length=255)
-    date = models.DateField(default=datetime.date.today)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     objects = UserManager()
